f5bbaf3827e0f492f150f7e78c1ffad9995a9fba
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-10-05 12:32:30 +0200

    update bg to new image with color profile

--- src/lib/sponsors/Sponsors.svelte
+++ src/lib/sponsors/Sponsors.svelte
@@ -1,3 +1,30 @@
+<script>
+	// Sponsor images
+	import leveluptuts from './images/leveluptuts.svg';
+
+	// Gold sponsors
+	let gold = [
+		{
+			url: 'https://leveluptutorials.com',
+			name: 'Level Up Tuts',
+			desc: 'Cutting-edge, focused & high quality video tutorials for web developers and designers',
+			image: leveluptuts
+		},
+		{
+			url: '',
+			name: '',
+			desc: '',
+			image: ''
+		},
+		{
+			url: '',
+			name: '',
+			desc: '',
+			image: ''
+		}
+	];
+</script>
+
 <section class="stack" id="sponsors">
 	<h2>Sponsors</h2>
 	<hr />
@@ -6,10 +33,21 @@
 		<a href="/sponsors" class="platinum sponsor">Platinum 💎</a>
 	</div>
 	<div class="sponsors golds">
-		<!-- <a href="/sponsors" class="gold sponsor"><img src={svelteschool} alt="Svelte School Sponsor" /></a> -->
-		<a href="/sponsors" class="gold sponsor">Gold 🥇</a>
-		<a href="/sponsors" class="gold sponsor">Gold 🥇</a>
-		<a href="/sponsors" class="gold sponsor">Gold 🥇</a>
+		{#each gold as { url, name, desc, image }}
+			{#if name}
+				<a
+					href={url}
+					class="gold sponsor"
+					rel="noopener noreferrer"
+					target="_blank"
+					data-tooltip={desc}
+				>
+					<img src={image} alt="{name} logo" />
+				</a>
+			{:else}
+				<a href="/sponsors" class="gold sponsor">Gold 🥇</a>
+			{/if}
+		{/each}
 	</div>
 	<div class="sponsors silvers">
 		<!-- <a href="/sponsors" class="silver sponsor"><img src={svelteschool} alt="Svelte School Sponsor" /></a> -->
@@ -65,5 +103,50 @@
 
 	h3 {
 		margin-top: 2.5rem;
+	}
+
+	[data-tooltip] {
+		position: relative;
+	}
+	[data-tooltip]:before,
+	[data-tooltip]:after {
+		position: absolute;
+		visibility: hidden;
+		opacity: 0;
+		-webkit-transition: opacity 0.2s ease-in-out, visibility 0.2s ease-in-out,
+			-webkit-transform 0.2s cubic-bezier(0.71, 1.7, 0.77, 1.24);
+		-moz-transition: opacity 0.2s ease-in-out, visibility 0.2s ease-in-out,
+			-moz-transform 0.2s cubic-bezier(0.71, 1.7, 0.77, 1.24);
+		transition: opacity 0.2s ease-in-out, visibility 0.2s ease-in-out,
+			transform 0.2s cubic-bezier(0.71, 1.7, 0.77, 1.24);
+		-webkit-transform: translate3d(0, 0, 0);
+		-moz-transform: translate3d(0, 0, 0);
+		transform: translate3d(0, 0, 0);
+		pointer-events: none;
+	}
+	[data-tooltip]:hover:before,
+	[data-tooltip]:hover:after,
+	[data-tooltip]:focus:before,
+	[data-tooltip]:focus:after {
+		visibility: visible;
+		opacity: 1;
+	}
+	[data-tooltip]:after {
+		z-index: 1000;
+		padding: 8px;
+		width: 260px;
+		background-color: rgba(0, 0, 0, 1);
+		border-radius: 4px;
+		color: #fff;
+		font-weight: normal;
+		content: attr(data-tooltip);
+		font-size: 14px;
+		line-height: 1.2;
+	}
+	[data-tooltip]:before,
+	[data-tooltip]:after {
+		top: 105%;
+		left: 50%;
+		transform: translate(-50%, 0);
 	}
 </style>
