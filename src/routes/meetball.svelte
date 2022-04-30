d948b5af5e8a5f2c46ab9365cf84f4dcc0581115
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2022-04-30 14:33:55 +0200

    add meetball link

---
+++ src/routes/meetball.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+	export async function load() {
+		return {
+			status: 302,
+			redirect: 'https://app.meetball.live/f3b821ac-2aaf-4515-804a-1e0fae96381d'
+		};
+	}
+</script>
