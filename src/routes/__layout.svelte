ea0c6c4b311f1432c51e19ed1e9c6c6472a3759a
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2022-05-23 12:57:09 +0200

    new metatag info

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,35 +8,29 @@
 <svelte:head>
 	<!-- Primary Meta Tags -->
 	<title>Svelte Summit Fall 2022: The First In-person Svelte only conference</title>
-	<meta content="Svelte Summit Spring 2022: The 5th Svelte only conference" name="title" />
 	<meta
-		content="Svelte Summit is an online JavaScript event focused on Svelte and its community taking place on April 30th 2022."
-		name="description"
+		content="Svelte Summit Fall 2022: The first in-person Svelte only conference"
+		name="title"
 	/>
+	<meta content="The first in-person Svelte only conference." name="description" />
 
 	<!-- Open Graph / Facebook -->
 	<meta content="website" property="og:type" />
 	<meta content="https://sveltesummit.com/" property="og:url" />
-	<meta content="Svelte Summit Spring 2022: The 5th Svelte only conference" property="og:title" />
-	<meta
-		content="Svelte Summit is an online JavaScript event focused on Svelte and its community taking place on April 30th 2022."
-		property="og:description"
-	/>
-	<meta content="https://sveltesummit.com/metatagimg-spring.png?23826843" property="og:image" />
+	<meta content="Svelte Summit Fall 2022: The 5th Svelte only conference" property="og:title" />
+	<meta content="The first in-person Svelte only conference." property="og:description" />
+	<meta content="https://sveltesummit.com/metaimg-fall22.png?2382623234843" property="og:image" />
 
 	<!-- Twitter -->
 	<meta content="summary_large_image" property="twitter:card" />
 	<meta content="https://sveltesummit.com/" property="twitter:url" />
 	<meta
-		content="Svelte Summit Spring 2022: The 5th Svelte only conference"
+		content="Svelte Summit Fall 2022: The first in-person Svelte only conference"
 		property="twitter:title"
 	/>
+	<meta content="The first in-person Svelte only conference." property="twitter:description" />
 	<meta
-		content="Svelte Summit is an online JavaScript event focused on Svelte and its community taking place on April 30th 2022."
-		property="twitter:description"
-	/>
-	<meta
-		content="https://sveltesummit.com/metatagimg-spring.png?23826843"
+		content="https://sveltesummit.com/metaimg-fall22.png?23822342346843"
 		property="twitter:image"
 	/>
 </svelte:head>
